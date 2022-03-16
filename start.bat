e7fe55f70acb4e2924b140c27bc3fc1bfc066feb
vbssubscribeit <88543097+vbssubscribeit@users.noreply.github.com>
2022-03-16 13:53:02 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: @HarChanel
+echo Username: admin
+echo Password: @hashmaxtech
 echo Silahkan Login Ke RDP Anda!!
 ping -n 10 127.0.0.1 >nul
