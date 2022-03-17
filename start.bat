15ff7c4ec6dc885419e1bf70bdcac40ee90d2781
vbssubscribeit <88543097+vbssubscribeit@users.noreply.github.com>
2022-03-17 12:35:02 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -17,5 +17,5 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: admin
 echo Password: @hashmaxtech
-echo Silahkan Login Ke RDP Anda!!
+echo Please login to your RDP
 ping -n 10 127.0.0.1 >nul
